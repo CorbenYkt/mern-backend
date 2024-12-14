abc4ff8a435b8a3cad1c93e1de5cc0dc29391aad
CorbenYkt <117908636+CorbenYkt@users.noreply.github.com>
2024-12-15 12:12:06 +1300

    added comments functionality

---
+++ controlles/CommentController.js
@@ -0,0 +1,31 @@
+import CommentModel from "../models/Comment.js";
+
+export const createComment = async (req, res) => {
+  try {
+    const { postId, text } = req.body;
+    const userId = req.userId;
+
+    const newComment = new CommentModel({
+      postId,
+      userId,
+      text,
+    });
+
+    const savedComment = await newComment.save();
+    res.status(201).json(savedComment);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Error creating a comment" });
+  }
+};
+
+export const getCommentsByPost = async (req, res) => {
+  try {
+    const { postId } = req.params;
+    const comments = await CommentModel.find({ postId }).populate("userId", "fullName avatar");
+    res.status(200).json(comments);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Error retrieving all comments" });
+  }
+};
