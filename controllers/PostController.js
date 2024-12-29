e11fc2b0b8b36924eb215c2ba6a52c08972b51c4
CorbenYkt <117908636+CorbenYkt@users.noreply.github.com>
2024-12-30 12:08:07 +1300

    UPD: tags functionality added

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -19,20 +19,6 @@ export const getAll = async (req, res) => {
     }
 };
 
-// export const getTags = async (req, res) => {
-//     try {
-//         const posts = await PostModel.find().limit(5).exec();
-//         const tags = posts.map(obj => obj.tags.flat().slice(5, 5))
-
-//         res.json(tags)
-//     } catch (err) {
-//         console.log(err)
-//         res.status(500).json({
-//             message: 'Error fetching posts tags'
-//         })
-//     }
-// };
-
 export const getTags = async (req, res) => {
     try {
         const posts = await PostModel.find().exec();
